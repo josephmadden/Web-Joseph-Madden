d01e123526a7995747bea999f39cd121d393248c
josephmadden <josephmadden@live.co.uk>
2015-03-29 15:34:06 +0100

    Videos: Get Lifted Sessions | Photos: Amends

--- video.php
+++ video.php
@@ -5,23 +5,37 @@
 	$videos = array(
 
 		array(
-			"vimeo" => "120538042",
-			"data-title" => "Vimeo Test",
-			"thumbnail" => "https://f.vimeocdn.com/images_v6/logo.png",
-			"text-image" => "http://www.visiblemeasures.com/wp-content/uploads/2013/04/vimeo_logo_white_on_blue.jpg",
-			"data-description" => "Cinematography, editing and motion graphics by <em>Joseph Madden</em>"
+			"youtube" => "ELdbhSKiRo4",
+			"data-title" => "Keep On Going - Ebonie G | Get Lifted Sessions",
+			"thumbnail" => "/thumbs/vid-thumb-ebonie-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-getlifted.png",
+			"data-description" => "Soul singer and songwriter <a href='http://www.eboniegmusic.com/' target='_blank'>Ebonie G</a> performs 'Keep On Going' from her debut album 'I Am Beautiful Inspiration' as part of the <a href='https://www.youtube.com/user/getliftedLDN' target='_blank'>Get Lifted</a> Sessions series.</p>
+			<p>Directed and edited by Joseph Madden</p>
+			<p>Produced by Lewis Daniel for <a href='https://www.youtube.com/user/getliftedLDN' target='_blank'>Get Lifted TV</a></p>
+			<p>Camera Operator 1: Joseph Madden</p>
+			<p>Camera Operator 2/Stills Photographer: <a href='http://www.patricklarder.com/' target='_blank'>Patrick Larder</a></p>
+			<p>Sound recorded and mixed by Lewis Daniel</p>
+			<p>Executive Producers: Lewis Daniel & Adam Gray</p>"
 		),
 
 		array(
-			"youtube" => "ZsqpY2zBrGY",
-			"data-title" => "Joseph Madden - Showreel 2014",
-			"thumbnail" => "/thumbs/vid-thumb-showreel-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-showreel-3.png",
-			"data-description" => "Cinematography, editing and motion graphics by Joseph Madden"
+			"vimeo" => "121253734",
+			"data-title" => "Ore - Femi Temowo | Get Lifted Sessions",
+			"thumbnail" => "/thumbs/vid-thumb-femi-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-getlifted.png",
+			"data-description" => "
+			MOBO-nominated guitarist and former Music Director to Amy Winehouse, <a href='http://www.femiguitar.com/' target='_blank'>Femi Temowo</a>, sings 'Ore' as part of the <a href='https://www.youtube.com/user/getliftedLDN' target='_blank'>Get Lifted</a> Sessions series.</p>
+			<p>Directed and edited by Joseph Madden</p>
+			<p>Produced by Lewis Daniel for <a href='https://www.youtube.com/user/getliftedLDN' target='_blank'>Get Lifted TV</a></p>
+			<p>Camera Operator 1: Joseph Madden</p>
+			<p>Camera Operator 2: <a href='https://vimeo.com/ollieholder' target='_blank'>Ollie Holder</a>
+			<p>Stills Photographer: <a href='http://www.patricklarder.com/' target='_blank'>Patrick Larder</a></p>
+			<p>Sound recorded and mixed by Lewis Daniel</p>
+			<p>Executive Producers: Lewis Daniel & Adam Gray</p>"
 		),
 
 		array(
-			"youtube" => "dWrJqL9p5EY",
+			"vimeo" => "117582523",
 			"data-title" => "Misread Heartbreak - Sabrina (feat. Ricardo Hinds)",
 			"thumbnail" => "/thumbs/vid-thumb-sabrina-1.jpg",
 			"text-image" => "/thumbs/vid-thumb-sabrina-2.png",
@@ -31,14 +45,6 @@
 			<p>Assistant Director/Gaffer: <a href='http://www.patricklarder.com/' target='_blank'>Patrick Larder</a></p>
 			<p>Cinematographer: Dominic Ellis</p>
 			<p>Editor: Joseph Madden</p>"
-		),
-
-		array(
-			"youtube" => "VNAP1-HIWZY",
-			"data-title" => "London Fashion Magazine | ZERO Magazine",
-			"thumbnail" => "/thumbs/vid-thumb-lfw-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-lfw-2.png",
-			"data-description" => "Shot and edited by Joseph Madden for <a href='http://www.zero-magazine.co.uk/' target='_blank'>ZERO Magazine</a>"
 		),
 
 		array(
@@ -70,6 +76,55 @@
 		),
 
 		array(
+			"vimeo" => "120560871",
+			"data-title" => "London Fashion Magazine | ZERO Magazine",
+			"thumbnail" => "/thumbs/vid-thumb-lfw-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-lfw-2.png",
+			"data-description" => "Shot and edited by Joseph Madden for <a href='http://www.zero-magazine.co.uk/' target='_blank'>ZERO Magazine</a>"
+		),
+
+		array(
+			"youtube" => "MN6jiVf2iw0",
+			"data-title" => "BeyondMe Boot Camp 2014",
+			"thumbnail" => "/thumbs/vid-thumb-beyondme-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-beyondme-2.png",
+			"data-description" => "Produced by Anisha Patel for <a href='https://beyondme.org/' target='_blank'>BeyondMe</a></p>
+			<p>Shot and edited by Joseph Madden</p>"
+		),
+
+		array(
+			"youtube" => "ThelyihlVYU",
+			"data-title" => "B&Q Autumn Winter Press Show 2014",
+			"thumbnail" => "/thumbs/vid-thumb-awpress-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-awpress-2.png",
+			"data-description" => "Produced by Myrhh Fletcher for <a href='http://www.diy.com' target='_blank'>B&Q</a></p>
+			<p>Directed, shot and edited by Joseph Madden</p>
+			<p>Production assistant: Gemma Smith</p>
+			<p>Camera assistant: Ida Wibe Hagen</p>"
+		),
+
+		array(
+			"youtube" => "3r2VNCzv_xU",
+			"data-title" => "B&Q Spring Summer Press Show 2014",
+			"thumbnail" => "/thumbs/vid-thumb-bqpress-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-bqpress-2.png",
+			"data-description" => "Produced and directed by Stephanie Mann for <a href='http://www.diy.com' target='_blank'>B&Q</a></p>
+			<p>Shot and edited by Joseph Madden</p>
+			<p>Production assistant: Gemma Smith</p>"
+		),
+
+		array(
+			"youtube" => "NqHNnp7o8RM",
+			"data-title" => "Kingfisher's One Team Product Show",
+			"thumbnail" => "/thumbs/vid-thumb-otps-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-otps-2.png",
+			"data-description" => "I filmed and edited this as part of the coverage of <a href='http://www.kingfisher.com/index.asp?pageid=161&mediaid=809&startrow=1&category=all' target='_blank'>Kingfisher</a>'s 5 day event for its store employees in Lille, France, in October 2013.<p> Featuring interviews with Sir Ian Cheshire (Group CEO, Kingfisher Group), Kevin O' Byrne (CEO, B&Q UK & Ireland), Véronique Laury (CEO, Castorama France) and more.</p>
+			<p>Produced and directed by Nadia Bonomally</p>
+			<p>Shot and edited by Joseph Madden</p>
+			<p>Production assistant: Douglas Linssen</p>"
+		),
+
+		array(
 			"youtube" => "_x2cz_Z6hFk",
 			"data-title" => "IDA - Official Trailer (2014)",
 			"thumbnail" => "/thumbs/vid-thumb-ida-trailer-2.jpg",
@@ -85,55 +140,6 @@
 		),
 
 		array(
-			"youtube" => "3r2VNCzv_xU",
-			"data-title" => "B&Q Spring Summer Press Show 2014",
-			"thumbnail" => "/thumbs/vid-thumb-bqpress-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-bqpress-2.png",
-			"data-description" => "Produced and directed by Stephanie Mann for <a href='http://www.diy.com' target='_blank'>B&Q</a></p>
-			<p>Shot and edited by Joseph Madden</p>
-			<p>Production assistant: Gemma Smith</p>"
-		),
-
-		array(
-			"youtube" => "ThelyihlVYU",
-			"data-title" => "B&Q Autumn Winter Press Show 2014",
-			"thumbnail" => "/thumbs/vid-thumb-awpress-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-awpress-2.png",
-			"data-description" => "Produced by Myrhh Fletcher for <a href='http://www.diy.com' target='_blank'>B&Q</a></p>
-			<p>Directed, shot and edited by Joseph Madden</p>
-			<p>Production assistant: Gemma Smith</p>
-			<p>Camera assistant: Ida Wibe Hagen</p>"
-		),
-
-		array(
-			"youtube" => "NqHNnp7o8RM",
-			"data-title" => "Kingfisher's One Team Product Show",
-			"thumbnail" => "/thumbs/vid-thumb-otps-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-otps-2.png",
-			"data-description" => "I filmed and edited this as part of the coverage of <a href='http://www.kingfisher.com/index.asp?pageid=161&mediaid=809&startrow=1&category=all' target='_blank'>Kingfisher</a>'s 5 day event for its store employees in Lille, France, in October 2013.<p> Featuring interviews with Sir Ian Cheshire (Group CEO, Kingfisher Group), Kevin O' Byrne (CEO, B&Q UK & Ireland), Véronique Laury (CEO, Castorama France) and more.</p>
-			<p>Produced and directed by Nadia Bonomally</p>
-			<p>Shot and edited by Joseph Madden</p>
-			<p>Production assistant: Douglas Linssen</p>"
-		),
-
-		array(
-			"youtube" => "MN6jiVf2iw0",
-			"data-title" => "BeyondMe Boot Camp 2014",
-			"thumbnail" => "/thumbs/vid-thumb-beyondme-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-beyondme-2.png",
-			"data-description" => "Produced by Anisha Patel for <a href='https://beyondme.org/' target='_blank'>BeyondMe</a></p>
-			<p>Shot and edited by Joseph Madden</p>"
-		),
-
-		array(
-			"youtube" => "-H96m0ZKnNM",
-			"data-title" => "Lynnwood - Thomas Brown book launch",
-			"thumbnail" => "/thumbs/vid-thumb-tjb-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-tjb-2.png",
-			"data-description" => "I photographed Tom in late 2012 for the inside cover photo of his debut novel, <a href='http://tbrownonline.wordpress.com/books/' target='_blank'>Lynnwood</a>. So when he invited me to <a href='http://josephmadden.co.uk/photo' target='_blank'>photograph its launch at his local Waterstone's</a> the following June I was pretty excited, but definitely wasn’t expecting the kind of fanfare it received. The book sold out within the hour and I’ve never seen so many people in a bookshop at one time. An all round great day out in a beautiful little town in Oxfordshire.<p>I wasn't going to turn up and take photos without filming it as well, so made this little video of the event. Luckily the staff in Waterstone's were very friendly and let me set up camp behind the till with all my gear.</p>"
-		),
-
-		array(
 			"youtube" => "pz4PiR03uHY",
 			"data-title" => "Monkey Burgers",
 			"thumbnail" => "/thumbs/vid-thumb-monkey-burgers-1.jpg",
@@ -146,6 +152,18 @@
 			<p>Runners: Ida Wibe Hagen and George Brereton</p>"
 		),
 
+		array(
+			"youtube" => "9sRVFO_w1Zc",
+			"data-title" => "IDA - Teaser Trailer (2012)",
+			"thumbnail" => "/thumbs/vid-thumb-ida-teaser-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-ida-teaser-2.png",
+			"data-description" => "Written & directed by Joseph Madden</p>
+			<p>Produced by Joseph Madden and Ida Wibe Hagen for Smart Casual Pictures</p>
+			<p>Cinematography, editing and VFX by Joseph Madden</p>
+			<p>Starring Ida Wibe Hagen, Tom Camfield, Fern Harffey</p>
+			<p>Runner: <a href='http://chloephilippou.com' target='_blank'>Chloe Philippou</a>"
+		),
+		
 		array(
 			"youtube" => "cFfi1icNR6A",
 			"data-title" => "Short Fuse",
@@ -163,16 +181,21 @@
 		),
 
 		array(
-			"youtube" => "9sRVFO_w1Zc",
-			"data-title" => "IDA - Teaser Trailer (2012)",
-			"thumbnail" => "/thumbs/vid-thumb-ida-teaser-1.jpg",
-			"text-image" => "/thumbs/vid-thumb-ida-teaser-2.png",
-			"data-description" => "Written & directed by Joseph Madden</p>
-			<p>Produced by Joseph Madden and Ida Wibe Hagen for Smart Casual Pictures</p>
-			<p>Cinematography, editing and VFX by Joseph Madden</p>
-			<p>Starring Ida Wibe Hagen, Tom Camfield, Fern Harffey</p>
-			<p>Runner: <a href='http://chloephilippou.com' target='_blank'>Chloe Philippou</a>"
+			"youtube" => "-H96m0ZKnNM",
+			"data-title" => "Lynnwood - Thomas Brown book launch",
+			"thumbnail" => "/thumbs/vid-thumb-tjb-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-tjb-2.png",
+			"data-description" => "I photographed Tom in late 2012 for the inside cover photo of his debut novel, <a href='http://tbrownonline.wordpress.com/books/' target='_blank'>Lynnwood</a>. So when he invited me to <a href='http://josephmadden.co.uk/photo' target='_blank'>photograph its launch at his local Waterstone's</a> the following June I was pretty excited, but definitely wasn’t expecting the kind of fanfare it received. The book sold out within the hour and I’ve never seen so many people in a bookshop at one time. An all round great day out in a beautiful little town in Oxfordshire.<p>I wasn't going to turn up and take photos without filming it as well, so made this little video of the event. Luckily the staff in Waterstone's were very friendly and let me set up camp behind the till with all my gear.</p>"
+		),
+
+		array(
+			"youtube" => "ZsqpY2zBrGY",
+			"data-title" => "Joseph Madden - Showreel 2014",
+			"thumbnail" => "/thumbs/vid-thumb-showreel-1.jpg",
+			"text-image" => "/thumbs/vid-thumb-showreel-3.png",
+			"data-description" => "Cinematography, editing and motion graphics by Joseph Madden"
 		)
+
 	);
 ?>
 <script type="text/javascript">var videos = <?=json_encode($videos);?>;</script>
